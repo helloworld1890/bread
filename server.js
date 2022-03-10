eb03709745b44b5987c56e00414a612c04973ad9
Craig Caparaz <cappuccino1890@Craigs-Air.lan1>
2022-03-09 19:22:24 -0800

    css updated layout

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -16,16 +17,21 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
 
-// LISTEN
-app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
-})
-
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
-  
+
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
+
+    // 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log('nomming at port', PORT);
+})
--- views/index.jsx
+++ views/index.jsx
@@ -1,26 +1,22 @@
-const React = require('react')
-const Default = require('./layout/Default')
+const React = require("react");
+const Default = require("./layout/default");
 
-function Index ({breads, title})  {
-    return (
-      <Default title={title}>
-        {/* <h2>Index Page</h2>
+function Index({ breads, title }) {
+  return (
+    <Default title={title}>
+      {/* <h2>Index Page</h2>
         <p>I have {breads[0].name} bread!</p> */}
-     <ul>
-     {
-  breads.map((bread, index)=> {
-    return (
-      <li key={index}>
-        <a href={`/breads/${index}`}>
-          {bread.name}
-        </a>
-      </li>
-    )
-  })
-}
-     </ul>  
-      </Default>
-    )
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${index}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </Default>
+  );
 }
 
-module.exports = Index
+module.exports = Index;
